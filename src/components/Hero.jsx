ac520f0abb6627a67ed39be25dd6ea4fea2a3009
Arin0005 <arinthamke706@gmail.com>
2024-12-01 23:10:04 +0530

    final commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
                 variants={container(0)} 
                 initial="hidden"
                 animate="visible" 
-                className="pb-16 text-6xl font-thin tracking-tight  lg:mt-16 lg:text-8xl">Amar Pimple</motion.h1>
+                className="pb-16 text-6xl font-thin tracking-tight  lg:mt-16 lg:text-8xl">Arin Thamke</motion.h1>
 
                 <motion.span 
                 variants={container(0.5)} 
